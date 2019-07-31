d79ce415082e1678dc8a5cd36527ab193c6ce0c6
Anneke McGrady <annekemcgrady@gmail.com>
2019-07-31 11:59:47 -0600

    Fix images on animal cards

--- src/Card.js
+++ src/Card.js
@@ -4,8 +4,8 @@ import "./Card.css";
 const Card = ({ name, img, species, description }) => {
   return (
     <div className="animal-card">
-      <h3>{name}</h3>
       <img src={img} alt={species} />
+      <h3>{name}</h3>
       <h5>Species: {species}</h5>
       <p>{description}</p>
     </div>
